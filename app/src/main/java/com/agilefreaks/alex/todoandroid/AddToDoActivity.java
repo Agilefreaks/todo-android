aa407e9489a474018119b52a2ef7da126b613ef7
Unknown <alexcozma@live.com>
2018-07-19 12:01:46 +0300

    Display the added ToDo on main activity

--- app/src/main/java/com/agilefreaks/alex/todoandroid/AddToDoActivity.java
+++ app/src/main/java/com/agilefreaks/alex/todoandroid/AddToDoActivity.java
@@ -1,13 +1,33 @@
 package com.agilefreaks.alex.todoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddToDoActivity extends AppCompatActivity {
 
+    static final int CREATE_TODO = 1;
+    static final String TODO_TEXT = "todoText";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+    }
+
+    public void createTodo_onClick(View view)
+    {
+        EditText text = findViewById(R.id.addTodo_Text);
+
+        String todoDescription = text.getText().toString();
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(TODO_TEXT, todoDescription);
+        setResult(RESULT_OK,resultIntent);
+
+        finish();
     }
 }
--- app/src/main/java/com/agilefreaks/alex/todoandroid/MainActivity.java
+++ app/src/main/java/com/agilefreaks/alex/todoandroid/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int CREATE_TODO = 1;
+    static final String TODO_TEXT = "todoText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent addToDoIntent = new Intent(this,AddToDoActivity.class);
 
-        startActivity(addToDoIntent);
+        startActivityForResult(addToDoIntent, CREATE_TODO);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == RESULT_OK)
+        {
+            String text = data.getStringExtra(TODO_TEXT);
+
+            TextView txtv = findViewById(R.id.textview);
+
+            txtv.setText(text);
+        }
     }
 }
